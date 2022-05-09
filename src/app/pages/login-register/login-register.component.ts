ed06258c1a42e5f193ad3c2e8011e54a4608150c
PeerayuthNew <newter01@gmail.com>
2022-05-09 14:36:33 +0700

    update final project

--- src/app/pages/login-register/login-register.component.ts
+++ src/app/pages/login-register/login-register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-register',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-register.component.scss']
 })
 export class LoginRegisterComponent implements OnInit {
+  // ตัวแปรธรรมดา
+  message:string = "Hello Angular";
 
-  constructor() { }
+  // ตัวแปร object
+  profile = {
+    "name":"New",
+    "tel":"081132123",
+    "gender":"Male"
+  }
+
+  // 2 Way Data Bliding
+  userData = 
+    {
+      "email":"",
+      "password":""
+    }
+  
+
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
   }
-
+  submitLogin(){
+    if(this.userData.email == "admin@gmail.com" && this.userData.password == "1234")
+    {
+      //alert("Login Success");
+      this.router.navigate(['backend']);
+    }else{
+      alert("Login Fail!!!!!");
+    }
+  }
 }
